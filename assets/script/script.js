5fd3d6515573f8a68ef20332f455b6c110a4b9e4
alifmaulanarizqi <alifmaulana312@gmail.com>
2021-06-01 19:13:27 +0700

    menambahkan ukm section

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+
+  // sandwich icon
   $('.nav-button').click(function() {
     $('.nav-button').toggleClass('change');
   });
 
+  // custom navbar
   $(window).scroll(function(){
     let position = $(this).scrollTop();
     if(position >= 200) {
@@ -10,6 +13,15 @@ $(document).ready(function() {
     } else {
       $('.nav-menu').removeClass('custom-navbar');
     }
+  });
+
+  // carousel bottom
+  var heroCarousel = $("#heroCarousel");
+  var heroCarouselIndicators = $("#hero-carousel-indicators");
+  heroCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    (index === 0) ?
+    heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>"):
+      heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
   });
 
 });
